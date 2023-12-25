36a5fb7964a4bfdc3fb457253d71d1aa34f568f5
MaloGermond <github@matchalo.org>
2023-12-25 12:17:14 +0100

    [feat] Allows to interpolate colors

--- exemples/colors/index.js
+++ exemples/colors/index.js
@@ -8,18 +8,22 @@ let instance = {
 	size: {
 		w: 50,
 		h: 50
-	}
+	},
+	color: "#6472BA"
 }
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	//frameRate(1)
+	motion.to(instance.pos, { x: 250, y: 400 }, 1200, "easeInOut")
+	motion.to(instance, { color: "#91E939" }, 3000)
 
 }
 
 function draw() {
 	background("#F8F9FA");
 	rectMode(CENTER)
+	fill(instance.color)
 	rect(instance.pos.x, instance.pos.y, instance.size.w, instance.size.h, 12);
 
 	motion.motionHandler()
--- src/motion.js
+++ src/motion.js
@@ -9,9 +9,9 @@ const motion = (function() {
 
   /**
    * Add an animated object to motion
-   * @param {Object} obj - JS Object you want to target
-   * @param {Object} values - Parameters you want to reach
-   * @param {Int} duration - How long this animation will it be
+   * @param {Object} obj - JS Object subject of the animation
+   * @param {Object} values - Attibutes value to reach. - {a:value, b:value,...}
+   * @param {Int} duration - in ms
    * @param {String} ease - Ease animation
    * @param {Function} Callback - Function execute when animation ends
    */
@@ -48,18 +48,21 @@ const motion = (function() {
     // Convert this value to progress from 0 to 1
     const progress = Math.min(elapsedTime / item.duration, 1);
 
-    // Add ease progress
+    // Calculate ease progress
     const easedProgress = getEaseProgress(progress, item.ease);
 
     // For each keys add progress value calulated
     Object.keys(item.endValue)
       .map((attr) => {
-        // Calculate the value when it's interpolated with ease factor.
-        const interpolatedValue =
-          item.startValue[attr] +
-          (item.endValue[attr] - item.startValue[attr]) * easedProgress;
-        item.subject[attr] = interpolatedValue;
-
+        if(typeof(item.endValue[attr]) == "number") {
+          item.subject[attr] = animateNumber(item.startValue[attr], item.endValue[attr], easedProgress)
+          return
+        }
+        if(typeof(item.endValue[attr]) == "string") {
+          item.subject[attr] = animateColor(item.startValue[attr], item.endValue[attr], easedProgress)
+          return
+        }
+        console.error("Not supported value " + attr)
       })
 
     // Signal that the animation has finised
@@ -69,6 +72,44 @@ const motion = (function() {
         item.callback();
       }
     }
+  }
+
+  /**
+   * Return progress value of a number
+   * @param {attr} attribute - Value to be change
+   * @param {el} element - element that contains value
+   * @param {progress} progress - % of progression from 0 to 1
+   * @return {Number} - Interpolated value
+   */
+  function animateNumber(start, end, progress) {
+
+    // Calculate the value when it's interpolated with ease factor.
+    const interpolatedValue =
+      start +
+      (end - start) * progress;
+    return interpolatedValue
+  }
+
+  /**
+   * Return progress value of a number
+   * @param {attr} attribute - Value to be change
+   * @param {el} element - element that contains value
+   * @param {progress} progress - % of progression from 0 to 1
+   * @return {Number} - Interpolated value
+   */
+  function animateColor(start, end, progress) {
+    const hslStart = hexToHsl(start)
+    const hslEnd = hexToHsl(end)
+
+    // Calculate the hue when it's interpolated with ease factor.
+    const interpolatedHue = {
+      h: animateNumber(hslStart.h, hslEnd.h, progress),
+      s: animateNumber(hslStart.s, hslEnd.s, progress),
+      l: animateNumber(hslStart.l, hslEnd.l, progress)
+    }
+
+
+    return hslToHex(interpolatedHue.h, interpolatedHue.s, interpolatedHue.l)
   }
 
   function motionHandler() {
@@ -93,3 +134,117 @@ const motion = (function() {
   }
 
 })()
+
+//
+// Utility functions
+//
+
+function hexToHsl(string) {
+  // Remove the hash if present
+  const hex = string.replace(/^#/, '')
+
+  // Parse the hex color to RGB
+  let bigint = parseInt(hex, 16);
+  let r = (bigint >> 16) & 255;
+  let g = (bigint >> 8) & 255;
+  let b = bigint & 255;
+
+  // Normalize RGB values to be in the range [0, 1]
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  // Find the minimum and maximum values to determine the lightness
+  let max = Math.max(r, g, b);
+  let min = Math.min(r, g, b);
+
+  // Calculate the lightness
+  let l = (max + min) / 2;
+
+  // Calculate the saturation
+  let s = 0;
+  if(max !== min) {
+    s = l > 0.5 ? (max - min) / (2 - max - min) : (max - min) / (max + min);
+  }
+
+  // Calculate the hue
+  let h = 0;
+  if(max !== min) {
+    switch (max) {
+      case r:
+        h = (g - b) / (max - min) + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / (max - min) + 2;
+        break;
+      case b:
+        h = (r - g) / (max - min) + 4;
+        break;
+    }
+    h /= 6;
+  }
+
+  // Convert hue to degrees
+  h *= 360;
+
+  return { h, s, l };
+}
+
+function hslToHex(h, s, l) {
+  // Convert hue to degrees
+  h = (h % 360 + 360) % 360;
+
+  // Normalize saturation and lightness to be in the range [0, 1]
+  s = Math.max(0, Math.min(1, s));
+  l = Math.max(0, Math.min(1, l));
+
+  // Convert HSL to RGB
+  let c = (1 - Math.abs(2 * l - 1)) * s;
+  let x = c * (1 - Math.abs((h / 60) % 2 - 1));
+  let m = l - c / 2;
+
+  let r, g, b;
+  if(h >= 0 && h < 60) {
+    r = c;
+    g = x;
+    b = 0;
+  } else if(h >= 60 && h < 120) {
+    r = x;
+    g = c;
+    b = 0;
+  } else if(h >= 120 && h < 180) {
+    r = 0;
+    g = c;
+    b = x;
+  } else if(h >= 180 && h < 240) {
+    r = 0;
+    g = x;
+    b = c;
+  } else if(h >= 240 && h < 300) {
+    r = x;
+    g = 0;
+    b = c;
+  } else {
+    r = c;
+    g = 0;
+    b = x;
+  }
+
+  // Normalize RGB values to be in the range [0, 1]
+  r = (r + m) * 255;
+  g = (g + m) * 255;
+  b = (b + m) * 255;
+
+  // Convert RGB to hexadecimal
+  const hexR = Math.round(r)
+    .toString(16)
+    .padStart(2, '0');
+  const hexG = Math.round(g)
+    .toString(16)
+    .padStart(2, '0');
+  const hexB = Math.round(b)
+    .toString(16)
+    .padStart(2, '0');
+
+  return `#${hexR}${hexG}${hexB}`;
+}
