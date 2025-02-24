f09418ee1c57a34e382c23ef9f3b2298f2e38ea9
Malo Germond <github@matchalo.org>
2025-02-24 17:02:51 +0100

    :construction: Clean a bit the home page with web components

--- components/card.js
+++ components/card.js
@@ -2,23 +2,71 @@ class ProjectCard extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' }); // Shadow DOM
+    this.project = String(this.getAttribute('project')) || '';
+    const path = './exemples/' + this.project;
+    const slug = 'http://localhost:3000/exemples/' + this.project + '/index.html';
+
     this.shadowRoot.innerHTML = `
       <style>
         .card {
-          padding: 10px;
-          border-radius: 8px;
-          box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-          background: white;
+          display: flex;
+          flex-direction: column;
+          justify-content: end;
+          align-items: center;
+          padding: 0;
+          gap: 1rem;
+
+          width: 208px;
+          height: 290px;
+
+          background: #F3F3F3;
+          background-image: url("${path}/cover.png");
+          background-size: cover;
+          border: 4px solid #FFFFFF;
+          border-radius: 0.5rem;
         }
+
+        slot{
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          padding: 0.25rem 0.5rem;
+          margin: 1rem;
+          gap: 7px;
+          color: #1C75F1;
+
+          background: rgba(255, 255, 255, 0.6);
+          backdrop-filter: blur(2px);
+          /* Note: backdrop-filter has minimal browser support */
+          border-radius: 32px;
+        }
+
+        a {
+          opacity: 0;
+          transform: translateY(50%) scale(0.9);
+          transition: .25s;
+          transition-timing-function: ease-out;
+        }
+          
+        .card:hover a{
+          opacity: 1;
+          transform: translateY(0%) scale(1);
+        }
+
       </style>
-      <article class="card-project w-400">
-          <img src="./assets/thumbnails/grid-pattern.png" class="cover w-300" />
-          <h3><slot></slot></h3>
-          <a href="http://localhost:3000/exemples/halftone/index.html">Grid pattern</a>
-          <code>http://192.168.1.125:3000/exemples/grid pattern/index.html</code>
+      <article class="card">
+          <a href="${slug}"><slot></slot></a>
         </article>
     `;
   }
 }
 
-customElements.define('projectCard', ProjectCard);
+customElements.define('project-card', ProjectCard);
+
+/* Group 9 */
+
+// /* Inside auto layout */
+// flex: none;
+// order: 1;
+// flex-grow: 0;
+// z-index: 1;
