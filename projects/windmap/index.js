6e02cbeb0b0ac82904543069c33af244447d663f
Malo Germond <github@matchalo.org>
2025-07-10 14:04:02 +0200

    :sparkles: Now windmap is working

--- projects/windmap/index.js
+++ projects/windmap/index.js
@@ -1,21 +1,36 @@
 import { animate } from 'https://cdn.skypack.dev/popmotion';
 import { windmap } from './windmap.js';
 
-const field = windmap({ columns: 5, rows: 2 });
+const field = windmap({ width: 500, height: 800, columns: 50, rows: 30 });
 
 window.setup = function () {
   createCanvas(windowWidth, windowHeight);
-  field.addWind(20, 30, 200, 100);
-  field.addWind(230, 190, 180, 160);
   field.create();
 };
 
 window.draw = function () {
   background('#F3F9F7'); // Ajout d'un fond pour éviter les traînées
-  field.displayGrid();
-  field.displayWinds();
+  // field.displayGrid();
+  // field.displayWinds();
   field.displayWindmap();
   // console.log(
   //   field.distanceToSegment({ x: mouseX, y: mouseY }, { x: 20, y: 30 }, { x: 200, y: 100 })
   // );
+
+  if (mouseIsPressed) {
+    push();
+    stroke('#000');
+    line(newVectorWind.x, newVectorWind.y, mouseX, mouseY);
+    pop();
+  }
+};
+
+let newVectorWind = {};
+window.mousePressed = function () {
+  newVectorWind = createVector(mouseX, mouseY);
+};
+
+window.mouseReleased = function () {
+  field.addWind(newVectorWind.x, newVectorWind.y, mouseX, mouseY);
+  field.create();
 };
