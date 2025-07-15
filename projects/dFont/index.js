8313970573e5ae52bde0e7135ed175e883052324
Malo Germond <github@matchalo.org>
2025-07-15 11:43:03 +0200

    :construction: update dFont

--- projects/dFont/index.js
+++ projects/dFont/index.js
@@ -1,16 +1,17 @@
 import { animate } from 'https://cdn.skypack.dev/popmotion';
 
 const config = {
-  width: 500,
-  height: 500,
+  width: 1000,
+  height: 800,
   letter: {
     x: 100,
-    y: 480,
-    xHeight: 300,
+    y: 50,
+    xHeight: 20,
     unitWidth: 300,
     ascender: 400,
     barHeight: 200,
     weight: 50,
+    contrast: 4,
   },
 };
 
@@ -20,30 +21,20 @@ window.setup = function () {
 
 window.draw = function () {
   background('#F3F9F7'); // Ajout d'un fond pour éviter les traînées
-  config.letter.unitWidth = mouseX;
-  config.letter.ascender = mouseY;
+
+  const { x, y, unitWidth, ascender, weight, barHeight, xHeight, contrast } = config.letter;
+  config.letter.unitWidth = mouseX - x >= 0 ? mouseX - x : 0;
+  config.letter.ascender = height - mouseY;
+  push();
+  translate(x, config.height - y);
   noStroke();
   fill('#000');
-  rect(
-    config.letter.x,
-    config.letter.y,
-    config.letter.unitWidth,
-    -config.letter.ascender,
-    0,
-    0,
-    config.letter.unitWidth,
-    config.letter.unitWidth
-  );
+  rect(0, 0, unitWidth, -ascender, 0, 0, unitWidth, unitWidth);
 
   fill('#fff');
-  rect(
-    config.letter.x + config.letter.weight,
-    config.letter.y,
-    config.letter.unitWidth - 2 * config.letter.weight,
-    -config.letter.ascender + config.letter.weight,
-    0,
-    0,
-    config.letter.unitWidth,
-    config.letter.unitWidth
-  );
+  rect(weight, 0, unitWidth - 2 * weight, -ascender + weight, 0, 0, unitWidth, unitWidth);
+
+  fill('#000');
+  rect(0, -((xHeight * ascender) / 100), unitWidth, weight / contrast);
+  pop();
 };
